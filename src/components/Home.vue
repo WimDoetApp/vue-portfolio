586f2d6bcf7da4c8011b5037f0df402e7fa04e70
Wim Naudts <wimnaudts@yahoo.com>
2019-04-01 21:08:47 +0200

    resolve capital issues

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <div class="title-text">
       <h1 class="title is-1">wim's webspace</h1>
       <p class="intro-text">
-        <b>Hi.</b> My name is Wim Naudts, and I do <span class="underlined">computer stuff</span>.
+        <b>Hi.</b> My name is Wim Naudts, and I do <span class="underlined">computer stuff</span>
       </p>
       <p>I don't really have much to put here at the moment, so here's a nice picture:</p>
       <img alt="background" src="../assets/images/background.jpg" style="max-height: 500px; margin-top: 20px;">
